f21e04a132b1340ff1f87345d6ecc5b58f6f85c9
omarcespedes <omarcspds@gmail.com>
2017-11-03 11:59:42 -0400

    Solved with dp and bitmask

--- uva/uva10496/uva10496.cpp
+++ uva/uva10496/uva10496.cpp
@@ -1,19 +1,25 @@
 #include <cstdio>
 #include <algorithm>
 #include <cstdlib>
+#include <cstring>
 
 using namespace std;
 
 int dist[11][11];
 int x[12];
 int y[12];
+int dp[12][2049];
 int t = 0;
+int maxV;
 
 int tsp(int pos, int bitmask) {
     int m = 2147483000;
     int tmp;
-    if(t == pos + 1) {
+    if(bitmask == maxV) {
         return dist[0][pos];
+    }
+    if(dp[pos][bitmask]) {
+         return dp[pos][bitmask];
     }
 
     for(int i = 0 ; i < t ; i++) {
@@ -22,6 +28,8 @@ int tsp(int pos, int bitmask) {
             m = min(m, tmp);
         }
     }
+
+    dp[pos][bitmask] = m;
 
     return m;
 }
@@ -35,6 +43,7 @@ int main() {
         scanf("%d %d", &x[0], &y[0]);
         scanf("%d", &c);
         t = c + 1;
+        maxV = (1 << t) - 1;
         for(int i = 1 ; i <= c; i++) {
             scanf("%d %d", &x[i], &y[i]);
         }
@@ -43,7 +52,8 @@ int main() {
                 dist[i][j] = dist[j][i] =  abs(x[i] - x[j]) + abs(y[i] - y[j]);
             }
         }
-        printf("digito = %d", tsp(0,1));
+        memset(dp, 0, sizeof(dp));
+        printf("The shortest path has length %d\n", tsp(0,1));
     }
     return 0;
 }
